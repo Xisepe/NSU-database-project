144af8df4d7ded5db8080174fe35057cc7ff03cb
Xisepe <maxim.golubeff2014@gmail.com>
2024-04-14 17:35:23 +0700

    initial

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "NSU-database-project"
---
+++ src/main/kotlin/ru/ccfit/golubevm/nsudatabaseproject/NsuDatabaseProjectApplication.kt
@@ -0,0 +1,11 @@
+package ru.ccfit.golubevm.nsudatabaseproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NsuDatabaseProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<NsuDatabaseProjectApplication>(*args)
+}
